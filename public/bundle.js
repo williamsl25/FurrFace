d3674a602e67ca90ed3b3e1e50b77e93ba46b7e1
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-11-21 12:04:39 -0500

    updated input fields and template

--- public/bundle.js
+++ public/bundle.js
@@ -12975,7 +12975,7 @@ module.exports = Backbone.Router.extend({
 module.exports = {
   pet: [
     '<img src="<%= imageURL %>"><h3><%= petName %></h3>',
-    '<h4>,<%= petAge %></h4>',
+    '<h4><%= petAge %></h4>',
     '<p><%= neighborhood %></p>',
     '<p><%= aboutMe %></p>',
 
@@ -12984,12 +12984,12 @@ module.exports = {
   newUserForm: [
       '<form class= "petForm" action="addUser" enctype="multipart/form-data" method="post">',
         '<input type="text" name="username" class="form-control" id="username" placeholder="Username"><br>',
-        '<input type="text" name="password" class="form-control" id="password" placeholder="Password"><br> ',
+        '<input type="password" name="password" class="form-control" id="password" placeholder="Password"><br> ',
         '<h3>Now is the time to introduce your FurrFace!</h3>',
         '<input type="text" name="imageURL" class="form-control" id="imageURL" placeholder="Paste an image of your pet here!">',
         '<input type="text" name="petName" class="form-control" id="petName" placeholder="What is your pets name?">',
         '<input type="text" name="petType" class="form-control" id="petType" placeholder="What type of pet do you have?">',
-        '<input type="text" name="petAge" class="form-control" id="petAge" placeholder="How old is your pet?">',
+        '<input type="number" name="petAge" class="form-control" id="petAge" placeholder="How old is your pet?">',
         '<label for="male">What neighborhood do you live in?</label>',
         '<select name="selectNeighborhood">',
           '<option value="James Island">James Island</option>',
@@ -12999,7 +12999,7 @@ module.exports = {
           '<option value="Cannonborough">Cannonborough</option>',
           '<option value="Wagner Terrace">Wagner Terrace</option>',
         '</select>',
-        '<input type="text" name="aboutMe" id="aboutMe" class="form-control" placeholder="My Pets Interests">',
+        '<input type="text" name="aboutMe" id="aboutMe" class="form-control" placeholder="My Pet\'s Interests">',
       '<button type="submit" class="btn btn-default">Submit</button>',
       '</form>'
 
@@ -13009,7 +13009,7 @@ loginform: [
 
   '<form class= "loginForm" action="login" enctype="multipart/form-data" method="post">',
     '<input type="text" name="username" class="form-control" id="username" placeholder="Username"><br>',
-    '<input type="text" name="password" class="form-control" id="password" placeholder="Password"><br> ',
+    '<input type="password" name="password" class="form-control" id="password" placeholder="Password"><br> ',
     '<button type="submit" class="btn btn-default">Submit</button>',
     '</form>',
     '<a href="#newUser">Create New Login</a>'
--- public/templates.js
+++ public/templates.js
@@ -1,7 +1,7 @@
 module.exports = {
   pet: [
     '<img src="<%= imageURL %>"><h3><%= petName %></h3>',
-    '<h4>,<%= petAge %></h4>',
+    '<h4><%= petAge %></h4>',
     '<p><%= neighborhood %></p>',
     '<p><%= aboutMe %></p>',
 
@@ -10,12 +10,12 @@ module.exports = {
   newUserForm: [
       '<form class= "petForm" action="addUser" enctype="multipart/form-data" method="post">',
         '<input type="text" name="username" class="form-control" id="username" placeholder="Username"><br>',
-        '<input type="text" name="password" class="form-control" id="password" placeholder="Password"><br> ',
+        '<input type="password" name="password" class="form-control" id="password" placeholder="Password"><br> ',
         '<h3>Now is the time to introduce your FurrFace!</h3>',
         '<input type="text" name="imageURL" class="form-control" id="imageURL" placeholder="Paste an image of your pet here!">',
         '<input type="text" name="petName" class="form-control" id="petName" placeholder="What is your pets name?">',
         '<input type="text" name="petType" class="form-control" id="petType" placeholder="What type of pet do you have?">',
-        '<input type="text" name="petAge" class="form-control" id="petAge" placeholder="How old is your pet?">',
+        '<input type="number" name="petAge" class="form-control" id="petAge" placeholder="How old is your pet?">',
         '<label for="male">What neighborhood do you live in?</label>',
         '<select name="selectNeighborhood">',
           '<option value="James Island">James Island</option>',
@@ -25,7 +25,7 @@ module.exports = {
           '<option value="Cannonborough">Cannonborough</option>',
           '<option value="Wagner Terrace">Wagner Terrace</option>',
         '</select>',
-        '<input type="text" name="aboutMe" id="aboutMe" class="form-control" placeholder="My Pets Interests">',
+        '<input type="text" name="aboutMe" id="aboutMe" class="form-control" placeholder="My Pet\'s Interests">',
       '<button type="submit" class="btn btn-default">Submit</button>',
       '</form>'
 
@@ -35,7 +35,7 @@ loginform: [
 
   '<form class= "loginForm" action="login" enctype="multipart/form-data" method="post">',
     '<input type="text" name="username" class="form-control" id="username" placeholder="Username"><br>',
-    '<input type="text" name="password" class="form-control" id="password" placeholder="Password"><br> ',
+    '<input type="password" name="password" class="form-control" id="password" placeholder="Password"><br> ',
     '<button type="submit" class="btn btn-default">Submit</button>',
     '</form>',
     '<a href="#newUser">Create New Login</a>'
