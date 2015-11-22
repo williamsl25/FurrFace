ae65471426ac619e663497f025563db27e7348c7
Kate Wilson <wilsonkate.kw@gmail.com>
2015-11-21 20:10:02 -0500

    button

--- public/collectionView.js
+++ public/collectionView.js
@@ -9,6 +9,7 @@ module.exports = Backbone.View.extend({
   el: ".petView",
   initialize: function(){
     $('.petView').css('width', '65%');
+    $('#logout').removeClass('hidden');
     this.addAll();
   },
   addOne: function(petModel){
