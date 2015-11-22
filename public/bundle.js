99309901ca164ebea54ea90337377f62f3f675ed
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-11-22 17:16:27 -0500

    commented on work

--- public/bundle.js
+++ public/bundle.js
@@ -243,7 +243,7 @@ module.exports = Backbone.View.extend({
     event.preventDefault();
     var comment = $('input[name="thoughts"]').val();
     var commentedOn = this.model;
-    commentedOn.set({comments: comment});
+    commentedOn.set({comments: commentedOn.get('comments').push(commentedOn)});
     commentedOn.save();
     this.$('input').val("");
   },
